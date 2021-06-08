2233cfb87e57a6b3674712f5a1b95e783a1d7c0d
hollie-rawlings <hollie.rawlings@pharmagenesis.com>
2021-06-08 17:45:59 +0100

    Initial commit

--- Package creation code.R
+++ Package creation code.R
@@ -1,11 +1,13 @@
 library(devtools)
 library(fs)
 
-create_package("C:/Users/hollie.rawlings/Documents/Development/DEV projects/DEV projects/OPGgtheme")
-use_git() 
+#create_package("C:/Users/hollie.rawlings/Documents/Development/DEV projects/DEV projects/OPGgtheme")
+#use_git()
 
-load_all("C:/Users/hollie.rawlings/Documents/Development/DEV projects/DEV projects/OPGgtheme")
+load_all()
 
 OPGgtheme::theme_OPG_basic()
-
+check()
 use_package("ggplot2")
+use_mit_license("Hollie Rawlings")
+document()
--- R/theme_OPG_basic.R
+++ R/theme_OPG_basic.R
@@ -1,46 +1,51 @@
-
+#' A basic universal company theme
+#'
+#'Imports a ggplot 2 thgeme object to help you theme plot within company guidelines
+#'
+#'
+#'
+#'
+#'
+#'
 theme_OPG_basic <- function(){
   font <- "Georgia"   #assign font family up front
 
-  ggplot2:theme_minimal() +   #replace elements we want to change
+  ggplot2::theme_minimal() +   #replace elements we want to change
 
-    ggplot2:theme(
+    ggplot2::theme(
 
       #grid elements
-      panel.grid.major = element_blank(),    #strip major gridlines
-      panel.grid.minor = element_blank(),    #strip minor gridlines
-      axis.ticks = element_blank(),          #strip axis ticks
+      panel.grid.major = ggplot2::element_blank(),    #strip major gridlines
+      panel.grid.minor = ggplot2::element_blank(),    #strip minor gridlines
+      axis.ticks = ggplot2::element_blank(),          #strip axis ticks
 
       #since theme_minimal() already strips axis lines,
       #we don't need to do that again
 
       #text elements
-      plot.title = element_text(             #title
+      plot.title = ggplot2::element_text(             #title
         family = font,            #set font family
         size = 20,                #set font size
         face = 'bold',            #bold typeface
         hjust = 0,                #left align
         vjust = 2),               #raise slightly
 
-      plot.subtitle = element_text(          #subtitle
+      plot.subtitle = ggplot2::element_text(          #subtitle
         family = font,            #font family
         size = 14),               #font size
 
-      plot.caption = element_text(           #caption
+      plot.caption = ggplot2::element_text(           #caption
         family = font,            #font family
         size = 9,                 #font size
         hjust = 1),               #right align
 
-      axis.title = element_text(             #axis titles
+      axis.title = ggplot2::element_text(             #axis titles
         family = font,            #font family
         size = 10),               #font size
 
-      axis.text = element_text(              #axis text
+      axis.text = ggplot2::element_text(              #axis text
         family = font,            #axis famuly
-        size = 9),                #font size
-
-      axis.text.x = element_text(            #margin for axis text
-        margin=margin(5, b = 10))
+        size = 9)               #font size
 
       #since the legend often requires manual tweaking
       #based on plot content, don't define it here
